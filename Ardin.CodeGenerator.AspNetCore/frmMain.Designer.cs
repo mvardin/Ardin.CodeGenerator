b7d8ecbeb13bfb932eee17b74c186cf7a498bb41
Ardin <Ardin@gmail.com>
2021-12-11 11:01:23 +0330

    version 1

--- Ardin.CodeGenerator.AspNetCore/frmMain.Designer.cs
+++ Ardin.CodeGenerator.AspNetCore/frmMain.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Ardin.CodeGenerator.AspNetCore
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,13 +29,140 @@ namespace Ardin.CodeGenerator.AspNetCore
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNamespace = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrefix = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.cbTables = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGetTables = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Namespace: ";
+            // 
+            // txtNamespace
+            // 
+            this.txtNamespace.Location = new System.Drawing.Point(134, 21);
+            this.txtNamespace.Name = "txtNamespace";
+            this.txtNamespace.Size = new System.Drawing.Size(463, 23);
+            this.txtNamespace.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Prefix:";
+            // 
+            // txtPrefix
+            // 
+            this.txtPrefix.Location = new System.Drawing.Point(134, 61);
+            this.txtPrefix.Name = "txtPrefix";
+            this.txtPrefix.Size = new System.Drawing.Size(117, 23);
+            this.txtPrefix.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Connection String:";
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(134, 106);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(463, 23);
+            this.txtConnectionString.TabIndex = 1;
+            this.txtConnectionString.Text = "data source=.;initial catalog=database;user id=userid;password=password;MultipleA" +
+    "ctiveResultSets=True";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(237, 232);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(205, 50);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // cbTables
+            // 
+            this.cbTables.FormattingEnabled = true;
+            this.cbTables.Location = new System.Drawing.Point(134, 174);
+            this.cbTables.Name = "cbTables";
+            this.cbTables.Size = new System.Drawing.Size(463, 23);
+            this.cbTables.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Select Tables:";
+            // 
+            // btnGetTables
+            // 
+            this.btnGetTables.Location = new System.Drawing.Point(134, 135);
+            this.btnGetTables.Name = "btnGetTables";
+            this.btnGetTables.Size = new System.Drawing.Size(463, 26);
+            this.btnGetTables.TabIndex = 2;
+            this.btnGetTables.Text = "Get tables";
+            this.btnGetTables.UseVisualStyleBackColor = true;
+            this.btnGetTables.Click += new System.EventHandler(this.btnGetTables_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(645, 309);
+            this.Controls.Add(this.cbTables);
+            this.Controls.Add(this.btnGetTables);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtConnectionString);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrefix);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNamespace);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Ardin Code Generator for Asp.net Core";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNamespace;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrefix;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ComboBox cbTables;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGetTables;
     }
 }
 
--- Ardin.CodeGenerator.AspNetCore/frmMain.cs
+++ Ardin.CodeGenerator.AspNetCore/frmMain.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +13,294 @@ using System.Windows.Forms;
 
 namespace Ardin.CodeGenerator.AspNetCore
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        #region Props
+        private string[] standardColumns = new string[] { "InsertUserId", "InsertDateTime", "UpdateUserId", "UpdateDateTime", "Version" };
+        #endregion
+
+        #region Ctor
+        public frmMain()
         {
             InitializeComponent();
+            loadDataFromHistory();
+        }
+
+        private void loadDataFromHistory()
+        {
+            txtNamespace.Text = ConfigurationManager.AppSettings["Namespace"];
+            txtPrefix.Text = ConfigurationManager.AppSettings["Prefix"];
+            txtConnectionString.Text = ConfigurationManager.AppSettings["ConnectionString"];
+        }
+        #endregion
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(Path.Combine(folder.SelectedPath, "IBase.cs"), generateIBase());
+                File.WriteAllText(Path.Combine(folder.SelectedPath, txtPrefix.Text + "Context.cs"), generateContext());
+                File.WriteAllText(Path.Combine(folder.SelectedPath, cbTables.Text + ".cs"), generateEO());
+                SaveToHistory();
+                MessageBox.Show("Done ;)");
+            }
+        }
+        private void SaveToHistory()
+        {
+            System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings.Remove("Namespace");
+            config.AppSettings.Settings.Add("Namespace", txtNamespace.Text);
+
+            config.AppSettings.Settings.Remove("Prefix");
+            config.AppSettings.Settings.Add("Prefix", txtPrefix.Text);
+
+            config.AppSettings.Settings.Remove("ConnectionString");
+            config.AppSettings.Settings.Add("ConnectionString", txtConnectionString.Text);
+
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+            config.Save(ConfigurationSaveMode.Minimal);
+        }
+
+        private void btnGetTables_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(txtConnectionString.Text);
+                conn.Open();
+                DataTable table = conn.GetSchema("Tables");
+                cbTables.Items.Clear();
+                foreach (System.Data.DataRow row in table.Rows)
+                {
+                    foreach (System.Data.DataColumn col in table.Columns)
+                    {
+                        if (col.ColumnName == "TABLE_NAME")
+                        {
+                            cbTables.Items.Add(row[col]);
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(getExceptionMessage(ex));
+            }
+        }
+
+        private string generateEO()
+        {
+            string path = Application.StartupPath + "\\Pattern\\Models.txt";
+            FileInfo fi = new FileInfo(path);
+            StreamReader sr = fi.OpenText();
+            string retVal = sr.ReadToEnd();
+            sr.Dispose();
+
+            string loop = retVal.Remove(0, retVal.IndexOf("<BLOX::Loop::BLOXColumns>") + "<BLOX::Loop::BLOXColumns>".Length);
+            loop = loop.Remove(loop.IndexOf("</BLOX::Loop::BLOXColumns>"));
+
+            List<KeyValuePair<string, string>> columns = getColumns(cbTables.Text);
+            string columnsManipulates = string.Empty;
+            foreach (var column in columns)
+            {
+                if (isNotStandardColumns(column.Key))
+                {
+                    if (isPrimaryKeyColumn(column.Key, cbTables.Text))
+                    {
+                        string annotation = "[System.ComponentModel.DataAnnotations.Key]";
+                        columnsManipulates += annotation + "\r\n";
+                        columnsManipulates += loop.Replace("BLOXColumnName", column.Key).Replace("BLOXColumnType", GetClrType(column.Value));
+                    }
+                    else
+                    {
+                        columnsManipulates += loop.Replace("BLOXColumnName", column.Key).Replace("BLOXColumnType", GetClrType(column.Value));
+                    }
+                }
+            }
+
+            retVal = retVal.Replace("BLOXTableName", cbTables.Text);
+            retVal = retVal.Replace("BLOXNameSpace", txtNamespace.Text);
+            retVal = retVal.Replace("BLOXPrefix", txtPrefix.Text);
+            retVal = retVal.Replace(loop, columnsManipulates);
+            retVal = retVal.Replace("<BLOX::Loop::BLOXColumns>", string.Empty);
+            retVal = retVal.Replace("</BLOX::Loop::BLOXColumns>", string.Empty);
+            return retVal;
+        }
+        private string generateIBase()
+        {
+            string path = Application.StartupPath + "\\Pattern\\Ibase.txt";
+            FileInfo fi = new FileInfo(path);
+            StreamReader sr = fi.OpenText();
+            string retVal = sr.ReadToEnd();
+            sr.Dispose();
+
+            retVal = retVal.Replace("BLOXNameSpace", txtNamespace.Text);
+
+            return retVal;
+        }
+        private string generateContext()
+        {
+            string path = Application.StartupPath + "\\Pattern\\Context.txt";
+            FileInfo fi = new FileInfo(path);
+            StreamReader sr = fi.OpenText();
+            string retVal = sr.ReadToEnd();
+            sr.Dispose();
+
+            string loop = retVal.Remove(0, retVal.IndexOf("<BLOX::Loop::TableForContext>") + "<BLOX::Loop::TableForContext>".Length);
+            loop = loop.Remove(loop.IndexOf("</BLOX::Loop::TableForContext>"));
+
+            List<string> tables = getTables();
+            string tablesManipulates = string.Empty;
+            foreach (var table in tables)
+                tablesManipulates += loop.Replace("BLOXTableName", table);
+
+            retVal = retVal.Replace("BLOXNameSpace", txtNamespace.Text);
+            retVal = retVal.Replace("BLOXPrefix", txtPrefix.Text);
+            retVal = retVal.Replace(loop, tablesManipulates);
+            retVal = retVal.Replace("<BLOX::Loop::TableForContext>", string.Empty);
+            retVal = retVal.Replace("</BLOX::Loop::TableForContext>", string.Empty);
+            return retVal;
+        }
+        private bool isPrimaryKeyColumn(string column, string tableName)
+        {
+            return column.Equals(tableName + "Id");
+        }
+        private bool isNotStandardColumns(string column)
+        {
+            return !standardColumns.Contains(column);
+        }
+        private List<KeyValuePair<string, string>> getColumns(string tableName)
+        {
+            List<KeyValuePair<string, string>> columns = new List<KeyValuePair<string, string>>();
+            SqlConnection conn = new SqlConnection(txtConnectionString.Text);
+            conn.Open();
+
+            String[] columnRestrictions = new String[4];
+            DataTable departmentIDSchemaTable = conn.GetSchema("Columns", columnRestrictions);
+            var selectedRows = from info in departmentIDSchemaTable.AsEnumerable()
+                               select new
+                               {
+                                   TableCatalog = info["TABLE_CATALOG"],
+                                   TableSchema = info["TABLE_SCHEMA"],
+                                   TableName = info["TABLE_NAME"],
+                                   ColumnName = info["COLUMN_NAME"],
+                                   DataType = info["DATA_TYPE"]
+                               };
+            var columnNames = selectedRows.Where(a => a.TableName.ToString().Equals(cbTables.Text));
+            foreach (var row in columnNames)
+            {
+                columns.Add(new KeyValuePair<string, string>(row.ColumnName.ToString(), row.DataType.ToString()));
+            }
+            return columns;
+        }
+        private List<string> getTables()
+        {
+            List<string> tables = new List<string>();
+            SqlConnection conn = new SqlConnection(txtConnectionString.Text);
+            conn.Open();
+            DataTable table = conn.GetSchema("Tables");
+            foreach (System.Data.DataRow row in table.Rows)
+            {
+                foreach (System.Data.DataColumn col in table.Columns)
+                {
+                    if (col.ColumnName == "TABLE_NAME")
+                    {
+                        tables.Add(row[col].ToString());
+                        break;
+                    }
+                }
+            }
+            return tables;
+        }
+        public string GetClrType(string sqlType)
+        {
+            switch (sqlType)
+            {
+                case "bigint":
+                    return "long";
+
+                case "binary":
+                case "image":
+                case "timestamp":
+                case "varbinary":
+                    return "byte[]";
+
+                case "bit":
+                    return "bool";
+
+                case "char":
+                case "nchar":
+                case "ntext":
+                case "nvarchar":
+                case "text":
+                case "varchar":
+                case "xml":
+                    return "string";
+
+                case "datetime":
+                case "smalldatetime":
+                case "date":
+                case "time":
+                case "datetime2":
+                    return "DateTime";
+
+                case "decimal":
+                case "money":
+                case "smallmoney":
+                    return "decimal";
+
+                case "float":
+                    return "double";
+
+                case "int":
+                    return "int";
+
+                case "real":
+                    return "float";
+
+                case "uniqueidentifier":
+                    return "Guid";
+
+                case "smallint":
+                    return "short";
+
+                case "tinyint":
+                    return "byte";
+
+                case "variant":
+                case "udt":
+                    return "object";
+
+                case "structured":
+                    return "DataTable";
+
+                case "datetimeoffset":
+                    return "DateTimeOffset";
+
+                case "geography":
+                    return "Location";
+
+                default:
+                    throw new ArgumentOutOfRangeException("sqlType " + sqlType);
+            }
+        }
+        private string getExceptionMessage(Exception ex)
+        {
+            string message = ex.Message;
+            if (ex.InnerException != null)
+            {
+                message += "\r\n" + ex.InnerException.Message;
+                if (ex.InnerException.InnerException != null)
+                {
+                    message += "\r\n" + ex.InnerException.InnerException.Message;
+                    if (ex.InnerException.InnerException.InnerException != null)
+                    {
+                        message += "\r\n" + ex.InnerException.InnerException.InnerException.Message;
+                    }
+                }
+            }
+            return message;
         }
 
     }
